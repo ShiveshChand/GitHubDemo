9831bcdba71dba286fcc9f9931cf98155e3c9da2
ShiveshChand <shivesh.pathak@careeco.net>
2022-08-18 13:32:05 +0530

    18/08/22 add only one line for print

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("I am try to push our code on github");
+        System.out.println("I am changing code and pushing again our code with help of Powershell ");
     }
 
 }
